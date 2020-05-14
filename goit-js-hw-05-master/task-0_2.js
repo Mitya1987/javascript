3254bd71afd33f5268c5edc27769bc400270ec6a
Mitya1987z <mitya1987z@gmail.com>
2020-05-14 14:40:32 +0300

    did task 1-2

---
+++ goit-js-hw-05-master/task-0_2.js
@@ -0,0 +1,26 @@
+class User {
+  constructor(obj) {
+    this.obj = obj;
+  }
+  getInfo() {
+    console.log(
+      `User ${this.obj.name} is ${this.obj.age} years old and has ${this.obj.followers} followers`,
+    );
+  }
+}
+
+const mango = new User({
+  name: 'Mango',
+  age: 2,
+  followers: 20,
+});
+
+mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+const poly = new User({
+  name: 'Poly',
+  age: 3,
+  followers: 17,
+});
+
+poly.getInfo(); // User Poly is 3 years old and has 17 followers
---
+++ goit-js-hw-05-master/task-0_3.js
@@ -0,0 +1,34 @@
+class Storage {
+  constructor(arrayProduct) {
+    this.arrayProduct = arrayProduct;
+  }
+  getItems() {
+    return this.arrayProduct;
+  }
+
+  addItem(item) {
+    items.push(item);
+    console.log(items);
+  }
+
+  removeItem(item) {
+    items.splice(1, 1);
+    console.log(items);
+  }
+}
+
+const storage = new Storage([
+  'Нанитоиды',
+  'Пролонгер',
+  'Железные жупи',
+  'Антигравитатор',
+]);
+
+const items = storage.getItems();
+console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+storage.addItem('Дроид');
+console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+storage.removeItem('Пролонгер');
+console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
